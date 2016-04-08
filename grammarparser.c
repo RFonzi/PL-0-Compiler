9ad2660641a006bd5ef6dbe8c8210ca3adfdaae7
RFonzi <ryan.fonzi@gmail.com>
2016-04-08 10:27:41 -0400

    added statement

--- grammarparser.c
+++ grammarparser.c
@@ -10,12 +10,12 @@ void program(Token *tokenlist)
 {
 	Token * tempList = tokenList;
 	block (&tempList);
-	
+
 	if (tempList->type != periodsym)
 	{
 		// period missing error
 	}
-		
+
 }
 
 void block (Token *tempList)
@@ -37,7 +37,7 @@ void block (Token *tempList)
 		{
 			//error
 		}
-			
+
 		tempList = tempList->next;
 		while (tempList->type == commasym)
 		{
@@ -67,70 +67,71 @@ void block (Token *tempList)
 	if (tempList->type = varsym)
 	{
 		tempList = tempList->next;
-		
+
 		if (tempList->type != identsym)
 		{
 			// error
 		}
-		
+
 		tempList = tempList->next;
-		
+
 		while (tempList->type == commasym)
 		{
 			tempList = tempList->next;
-		
+
 			if (tempList->type != identsym)
 			{
 				// error
 			}
-		
+
 			tempList = tempList->next;
 		}
-		
+
 		if (tempList->type != semicolonsym)
 		{
 			// missing semi colon error
 		}
-		
+
 		tempList = tempList->next;
 	}
 	while (tempList->type == procsym)
 	{
 		tempList = tempList->next;
-		
+
 		if (tempList->type != identsym)
 		{
 			// error
 		}
-		
+
 		tempList = tempList->next;
-		
+
 		if (tempList->type != semicolonsym)
 		{
 			// error missing semi colon
 		}
-		
+
 		tempList = tempList->next;
-		
+
 		block(&tempList);
-		
+
 		tempList = tempList->next;
-		
+
 		if (tempList->type != semicolonsym)
 		{
 			// error missing semi colon
 		}
-		
+
 	}
-	
+
 	statement (&tempList);
 }
 
 
+<<<<<<< Updated upstream
 void condition (Token *tempList)
 {
 	tempList = tempList->next;
-	
+
 	if (tempList->type == oddsym)
 	{
 		tempList = tempList->next;
@@ -143,13 +144,72 @@ void condition (Token *tempList)
 		{
 			// error
 		}
-		
+
 		tempList = tempList->next;
 		expression(&tempList);
 	}
 }
 
+=======
+void statement(Token *tempList){
+	if(tempList->type == identsym){
+		tempList = tempList->next;
 
+		if(tempList->type != becomessym) //EQ instead of becomes??
+
+		tempList = tempList->next;
+
+		expression(tempList);
+	}
+	else if(tempList->type == callsym){
+		tempList = tempList->next;
+
+		if(tempList->type != identsym)
+			error(IDENT_MUST_FOLLOW_CALL);
+
+		tempList = tempList->next;
+	}
+	else if(tempList->type == beginsym){
+		tempList = tempList->next;
+
+		statement(tempList);
+
+		while(tempList->type == semicolonsym){
+			tempList = tempList->next;
+			statement(tempList);
+		}
+
+		if(tempList->type != endsym)
+			error(INCORRECT_SYM_FOLLOWING_STATEMENT) //Might need to change this
+
+		tempList = tempList->next; //MAY BE THE PERIOD AFTER endsym
+	}
+	else if(tempList->type == ifsym){
+		tempList = tempList->next;
+
+		condition(tempList);
+
+		if(tempList->type != thensym)
+			error(THEN_EXPECTED);
+
+		tempList = tempList->next;
+
+		statement(tempList);
+	}
+	else if(tempList->type == whilesym){
+		tempList = tempList->next;
+
+		condition(tempList);
+
+		if(tempList != dosym)
+			error(DO_EXPECTED);
+
+		tempList = tempList->next;
+
+		statement(tempList);
+	}
+
+}
 
 int relation (Token *tempList)
 {
@@ -165,12 +225,6 @@ int relation (Token *tempList)
 		return 1;
 	if (tempList->type == geqsym)
 		return 1;
-	
+
 	return 0;
 }
-
-
-
-
-
-
